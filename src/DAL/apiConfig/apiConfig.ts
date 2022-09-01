85b10732d7fb76d4f0887b6cbd1563f981096254
Denis Tavabilov <dagger1233@gmail.com>
2022-09-01 22:18:14 +0500

    fix

--- src/DAL/apiConfig/apiConfig.ts
+++ src/DAL/apiConfig/apiConfig.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const apiConfig = axios.create({
   withCredentials: true,
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL:
+    process.env.REACT_APP_BASE_URL || 'https://social-network.samuraijs.com/api/1.1',
   headers: {
-    // @ts-ignore
-    'API-KEY': process.env.REACT_APP_API_KEY,
+    'API-KEY': process.env.REACT_APP_API_KEY || 'd6a9965b-9508-4ee9-9d27-31daa8d146f5',
   },
 });
